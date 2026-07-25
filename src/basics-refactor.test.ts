ca62844a36b9854097aed72f8fc4d64fbfe3032b
ypmo <ypmo.dev@gmail.com>
2026-07-25 14:57:11 +0200

    feat: complete exercise 13

--- src/basics-refactor.test.ts
+++ src/basics-refactor.test.ts
@@ -338,23 +338,23 @@ Descobreix com tipar params com un objecte amb una clau first que sigui un nombr
 // Pista: utilitza typeof per diferenciar entre nombre i objecte.
 // */
 
-describe("Problema de filtratge amb typeof", () => {
-  const coerceAmount = (amount: number | { amount: number }) => {
-  if (typeof amount === "number") {
-    return amount;
-  }
-  return amount.amount;
-};
+// describe("Problema de filtratge amb typeof", () => {
+//   const coerceAmount = (amount: number | { amount: number }) => {
+//   if (typeof amount === "number") {
+//     return amount;
+//   }
+//   return amount.amount;
+// };
 
 
-  it("Ha de retornar l'import quan es passa un objecte", () => {
-    expect(coerceAmount({ amount: 20 })).toEqual(20);
-  });
+//   it("Ha de retornar l'import quan es passa un objecte", () => {
+//     expect(coerceAmount({ amount: 20 })).toEqual(20);
+//   });
 
-  it("Ha de retornar l'import quan es passa un nombre", () => {
-    expect(coerceAmount(20)).toEqual(20);
-  });
-});
+//   it("Ha de retornar l'import quan es passa un nombre", () => {
+//     expect(coerceAmount(20)).toEqual(20);
+//   });
+// });
 
 // /*
 // Repte 13:
@@ -362,21 +362,26 @@ describe("Problema de filtratge amb typeof", () => {
 // Pista: hi ha diverses maneres de resoldre aquest repte, prova diferents opcions!
 // */
 
-// describe("Problema de blocs catch", () => {
-//   const tryCatchDemo = (state: "fail" | "succeed") => {
-//     try {
-//       if (state === "fail") {
-//         throw new Error("Failure!");
-//       }
-//     } catch (e) {
-//       return e.message;
-//     }
-//   };
+describe("Problema de blocs catch", () => {
+  const tryCatchDemo = (state: "fail" | "succeed") => {
+    try {
+      if (state === "fail") {
+        throw new Error("Failure!");
+      }
+    } catch (e) {
+      if (e instanceof Error) {
+      return e.message;
+    }
+    }
+  };
 
-//   it("Ha de retornar el missatge quan falla", () => {
-//     expect(tryCatchDemo("fail")).toEqual("Failure!");
-//   });
-// });
+  it("Ha de retornar el missatge quan falla", () => {
+    expect(tryCatchDemo("fail")).toEqual("Failure!");
+  });
+});
+
+
+
 
 // /*
 // Repte 14:
