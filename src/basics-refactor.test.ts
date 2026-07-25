2f8f5c741d258472bb33910cbed260f93843bf68
ypmo <ypmo.dev@gmail.com>
2026-07-25 13:47:08 +0200

    feat: complete exercise-07

--- src/basics-refactor.test.ts
+++ src/basics-refactor.test.ts
@@ -145,64 +145,64 @@ Descobreix com tipar params com un objecte amb una clau first que sigui un nombr
 // Actualitza la interfície User per restringir la propietat role a una de les opcions establertes.
 // El valor I_SHOULD_NOT_BE_ALLOWED hauria de provocar un error, eliminant la línia vermella sota el comentari // @ts-expect-error.
 // */
-describe("Problema d'unions", () => {
-  interface User {
-    id: number;
-    firstName: string;
-    lastName: string;
-//     /**
-//      * Com ens assegurem que role només sigui un dels següents:
-//      * - 'admin'
-//      * - 'user'
-//      * - 'super-admin'
-//      */
-    role:  "admin" | "user" | "super-admin";
-  }
-
-  const defaultUser: User = {
-    id: 1,
-    firstName: "Jen",
-    lastName: "Simmons",
-    // @ts-expect-error
-    role: "I_SHOULD_NOT_BE_ALLOWED",
-  };
-});
-
-// /*
-// Repte 7:
-// Has de solucionar aquest error de tipus esbrinant com representar arrays.
-// */
-// describe("Problema d'arrays", () => {
+// describe("Problema d'unions", () => {
 //   interface User {
 //     id: number;
 //     firstName: string;
 //     lastName: string;
-//     role: "admin" | "user" | "super-admin";
-//     posts: Post;
-//   }
-
-//   interface Post {
-//     id: number;
-//     title: string;
+// //     /**
+// //      * Com ens assegurem que role només sigui un dels següents:
+// //      * - 'admin'
+// //      * - 'user'
+// //      * - 'super-admin'
+// //      */
+//     role:  "admin" | "user" | "super-admin";
 //   }
 
 //   const defaultUser: User = {
 //     id: 1,
 //     firstName: "Jen",
 //     lastName: "Simmons",
-//     role: "admin",
-//     posts: [
-//       {
-//         id: 1,
-//         title: "Com menjo tant de formatge",
-//       },
-//       {
-//         id: 2,
-//         title: "Per què no menjo més verdures",
-//       },
-//     ],
+//     // @ts-expect-error
+//     role: "I_SHOULD_NOT_BE_ALLOWED",
 //   };
 // });
+
+// /*
+// Repte 7:
+// Has de solucionar aquest error de tipus esbrinant com representar arrays.
+// */
+describe("Problema d'arrays", () => {
+  interface User {
+    id: number;
+    firstName: string;
+    lastName: string;
+    role: "admin" | "user" | "super-admin";
+    posts: Post[];
+  }
+
+  interface Post {
+    id: number;
+    title: string;
+  }
+
+  const defaultUser: User = {
+    id: 1,
+    firstName: "Jen",
+    lastName: "Simmons",
+    role: "admin",
+    posts: [
+      {
+        id: 1,
+        title: "Com menjo tant de formatge",
+      },
+      {
+        id: 2,
+        title: "Per què no menjo més verdures",
+      },
+    ],
+  };
+});
 
 // /*
 // Repte 8:
