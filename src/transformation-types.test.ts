74bccdead65ab2f28fdbfabcb610aaad5969c0b5
ypmo <ypmo.dev@gmail.com>
2026-07-25 18:12:40 +0200

    feat: complete transformation-types exercise-01

--- src/transformation-types.test.ts
+++ src/transformation-types.test.ts
@@ -1,23 +1,23 @@
-// import { describe, expect, it } from "vitest";
-// import { Equal, Expect } from "./helpers/type-utils";
+import { describe, expect, it } from "vitest";
+import { Equal, Expect } from "./helpers/type-utils";
 
-// /*
-// Repte 1:
-// Llegeix la documentació de TypeScript sobre Utility Types i esbrina com utilitzar ReturnType per extreure el tipus de retorn de myFunc.
-// Pista: pots utilitzar typeof per obtenir el tipus d'una variable o funció.
-// */
-// describe("Transformació: obtenir el tipus de retorn d'una funció", () => {
-//   const myFunc = () => {
-//     return "hello";
-//   };
+/*
+Repte 1:
+Llegeix la documentació de TypeScript sobre Utility Types i esbrina com utilitzar ReturnType per extreure el tipus de retorn de myFunc.
+Pista: pots utilitzar typeof per obtenir el tipus d'una variable o funció.
+*/
+describe("Transformació: obtenir el tipus de retorn d'una funció", () => {
+  const myFunc = () => {
+    return "hello";
+  };
 
-//   /**
-//    * Com podem extreure MyFuncReturn a partir de myFunc?
-//   //  */
-//   type MyFuncReturn = unknown;
+  /**
+   * Com podem extreure MyFuncReturn a partir de myFunc?
+  //  */
+  type MyFuncReturn = ReturnType<typeof myFunc>;
 
-//   type tests = [Expect<Equal<MyFuncReturn, string>>];
-// });
+  type tests = [Expect<Equal<MyFuncReturn, string>>];
+});
 
 // /*
 // Repte 2:
