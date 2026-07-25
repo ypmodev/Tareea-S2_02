086788023f3c2c2ed19e1c8b5a4bcd9a4abd7cd8
ypmo <ypmo.dev@gmail.com>
2026-07-25 14:09:34 +0200

    feat: complete exercise-11

--- src/basics-refactor.test.ts
+++ src/basics-refactor.test.ts
@@ -265,28 +265,28 @@ Descobreix com tipar params com un objecte amb una clau first que sigui un nombr
 // Actualitza guitarists perquè estigui tipat com un Set de strings.
 // */
 
-describe("Problema de Set", () => {
-  const guitarists = new Set<string>();
+// describe("Problema de Set", () => {
+//   const guitarists = new Set<string>();
 
-  guitarists.add("Jimi Hendrix");
-  guitarists.add("Eric Clapton");
+//   guitarists.add("Jimi Hendrix");
+//   guitarists.add("Eric Clapton");
 
-  it("Ha de contenir en Jimi i l'Eric", () => {
-    expect(guitarists.has("Jimi Hendrix")).toEqual(true);
-    expect(guitarists.has("Eric Clapton")).toEqual(true);
-  });
+//   it("Ha de contenir en Jimi i l'Eric", () => {
+//     expect(guitarists.has("Jimi Hendrix")).toEqual(true);
+//     expect(guitarists.has("Eric Clapton")).toEqual(true);
+//   });
 
-  it("Ha de donar un error de tipus si proves d'afegir un valor que no sigui string", () => {
-    // @ts-expect-error
-    guitarists.add(2);
-  });
+//   it("Ha de donar un error de tipus si proves d'afegir un valor que no sigui string", () => {
+//     // @ts-expect-error
+//     guitarists.add(2);
+//   });
 
-  it("Ha de ser un Set<string> que es pugui convertir a string[]", () => {
-    const guitaristsAsArray = Array.from(guitarists);
+//   it("Ha de ser un Set<string> que es pugui convertir a string[]", () => {
+//     const guitaristsAsArray = Array.from(guitarists);
 
-    type tests = [Expect<Equal<typeof guitaristsAsArray, string[]>>];
-  });
-});
+//     type tests = [Expect<Equal<typeof guitaristsAsArray, string[]>>];
+//   });
+// });
 
 // /*
 // Repte 11:
@@ -295,42 +295,42 @@ describe("Problema de Set", () => {
 // Pista: Consulta com representar objectes amb claus de tipus string i valors tipats.
 // */
 
-// describe("Problema de Record", () => {
-//   const createCache = () => {
-//     const cache = {};
+describe("Problema de Record", () => {
+  const createCache = () => {
+    const cache: Record<string, string> = {};
 
-//     const add = (id: string, value: string) => {
-//       cache[id] = value;
-//     };
+    const add = (id: string, value: string) => {
+      cache[id] = value;
+    };
 
-//     const remove = (id: string) => {
-//       delete cache[id];
-//     };
+    const remove = (id: string) => {
+      delete cache[id];
+    };
 
-//     return {
-//       cache,
-//       add,
-//       remove,
-//     };
-//   };
+    return {
+      cache,
+      add,
+      remove,
+    };
+  };
 
-//   it("Ha d'afegir valors a la memòria cache", () => {
-//     const cache = createCache();
+  it("Ha d'afegir valors a la memòria cache", () => {
+    const cache = createCache();
 
-//     cache.add("123", "Jen");
+    cache.add("123", "Jen");
 
-//     expect(cache.cache["123"]).toEqual("Jen");
-//   });
+    expect(cache.cache["123"]).toEqual("Jen");
+  });
 
-//   it("Ha d'eliminar valors de la memòria cache", () => {
-//     const cache = createCache();
+  it("Ha d'eliminar valors de la memòria cache", () => {
+    const cache = createCache();
 
-//     cache.add("123", "Jen");
-//     cache.remove("123");
+    cache.add("123", "Jen");
+    cache.remove("123");
 
-//     expect(cache.cache["123"]).toEqual(undefined);
-//   });
-// });
+    expect(cache.cache["123"]).toEqual(undefined);
+  });
+});
 
 // /*
 // Repte 12:
