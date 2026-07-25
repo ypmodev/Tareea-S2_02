3a7cf804bed8bd2140cfaed357dbaf1fadbfa377
ypmo <ypmo.dev@gmail.com>
2026-07-25 13:29:46 +0200

    feat: complete exercise-05

--- src/basics-refactor.test.ts
+++ src/basics-refactor.test.ts
@@ -86,54 +86,59 @@ Descobreix com tipar params com un objecte amb una clau first que sigui un nombr
 // Has d'esbrinar com marcar el paràmetre 'last' com a opcional.
 // */
 
-describe("Problema de paràmetres opcionals", () => {
-  const getName = (first: string, last?: string) => {
-    if (last !== undefined) {
-      return `${first} ${last}`;
-    }
-    return first;
-  };
+// describe("Problema de paràmetres opcionals", () => {
+//   const getName = (first: string, last?: string) => {
+//     if (last !== undefined) {
+//       return `${first} ${last}`;
+//     }
+//     return first;
+//   };
 
-  it("Ha de funcionar només amb el nom", () => {
-    const name = getName("Jen");
+//   it("Ha de funcionar només amb el nom", () => {
+//     const name = getName("Jen");
 
-    expect(name).toEqual("Jen");
-  });
+//     expect(name).toEqual("Jen");
+//   });
 
-  it("Ha de funcionar amb el nom i el cognom", () => {
-    const name = getName("Jen", "Simmons");
+//   it("Ha de funcionar amb el nom i el cognom", () => {
+//     const name = getName("Jen", "Simmons");
 
-    expect(name).toEqual("Jen Simmons");
-  });
-});
+//     expect(name).toEqual("Jen Simmons");
+//   });
+// });
 
 // /*
 // Repte 5:
 // Consulta la [documentació de TypeScript](https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html) i determina com canviar defaultUser perquè el test passi.
 // */
 
-// describe("Problema d'assignació de tipus a variables", () => {
-//   interface User {
-//     id: number;
-//     firstName: string;
-//     lastName: string;
-//     isAdmin: boolean;
-//   }
+describe("Problema d'assignació de tipus a variables", () => {
+  interface User {
+    id: number;
+    firstName: string;
+    lastName: string;
+    isAdmin: boolean;
+  }
 
 //   /**
 //    * Com ens assegurem que defaultUser sigui de tipus User
 //    * EN AQUESTA LÍNIA - no més endavant al codi?
 //    */
-//   const defaultUser = {};
+  const defaultUser: User = {
+    id: 1,
+    firstName: "Jen",
+    lastName: "Simons",
+    isAdmin: false,
+  };
 
-//   const getUserId = (user: User) => {
-//     return user.id;
-//   };
+  const getUserId = (user: User) => {
+    return user.id;
+  };
 
-//   it("Ha d'obtenir l'identificador de l'usuari", () => {
-//     expect(getUserId(defaultUser)).toEqual(1);
-//   });
-// });
+  it("Ha d'obtenir l'identificador de l'usuari", () => {
+    expect(getUserId(defaultUser)).toEqual(1);
+  });
+});
 
 // /*
 // Repte 6:
