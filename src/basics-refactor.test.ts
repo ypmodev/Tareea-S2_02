fb3f8126a0d64ae467e1cce2b6e6b0eb258fdc7e
ypmo <ypmo.dev@gmail.com>
2026-07-25 12:12:36 +0200

    feat: complete exercise-02

--- src/basics-refactor.test.ts
+++ src/basics-refactor.test.ts
@@ -8,44 +8,47 @@ Llegeix l'article:
  i mira si pots trobar com solucionar aquests errors de TypeScript.
 */
 
-describe("Problema de nombres", () => {
-  it("Ha de sumar els dos nombres", () => {
-    const addTwoNumbers = (a: number, b: number) => {
-      return a + b;
-    };
-    expectTypeOf(addTwoNumbers).parameter(0).toEqualTypeOf<number>();
-    expectTypeOf(addTwoNumbers).parameter(1).toEqualTypeOf<number>();
-  });
-});
+// describe("Problema de nombres", () => {
+//   it("Ha de sumar els dos nombres", () => {
+//     const addTwoNumbers = (a: number, b: number) => {
+//       return a + b;
+//     };
+//     expectTypeOf(addTwoNumbers).parameter(0).toEqualTypeOf<number>();
+//     expectTypeOf(addTwoNumbers).parameter(1).toEqualTypeOf<number>();
+//   });
+// });
 
 /*
 Repte 2:
 Descobreix com tipar params com un objecte amb una clau first que sigui un nombre i una clau second que també sigui un nombre.
 */
 
-// describe("Problema de paràmetre objecte", () => {
-//   it("Ha de sumar els dos nombres", () => {
-//     const addTwoNumbers = (params:{first:number,second:number}) => {
-//       return params.first + params.second;
-//     };
-//     expect(
-//       addTwoNumbers({
-//         first: 2,
-//         second: 4,
-//       })
-//     ).toEqual(6);
+describe("Problema de paràmetre objecte", () => {
 
-//     expect(
-//       addTwoNumbers({
-//         first: 10,
-//         second: 20,
-//       })
-//     ).toEqual(30);
-//   });
-//   expectTypeOf(addTwoNumbers).parameter(0).toEqualTypeOf<{ first: number; second: number }>();
+    const addTwoNumbers = (params: { first: number; second: number }) => {
+        return params.first + params.second;
+    };
 
 
-// });
+  it("Ha de sumar els dos nombres", () => {
+      expect(
+      addTwoNumbers({
+        first: 2,
+        second: 4,
+      })
+    ).toEqual(6);
+
+    expect(
+      addTwoNumbers({
+        first: 10,
+        second: 20,
+      })
+    ).toEqual(30);
+  });
+  expectTypeOf(addTwoNumbers).parameter(0).toEqualTypeOf<{ first: number; second: number }>();
+
+
+});
 
 // /*
 // Repte 3:
